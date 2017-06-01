3e74968f9e1fa6e84dabe3160d07edf7c8b9fcf1
Ian Deans <ideans715@gmail.com>
2017-05-31 17:08:44 -0700

    follow tutorial and set up game

---
+++ Level.js
@@ -0,0 +1,78 @@
+function Level( plan ) {
+    this.width = plan[0].length
+    this.height = plan.length
+    this.grid = []
+    this.actors = []
+
+    for ( var y = 0; y < this.height; y++ ) {
+        var line = plan[y], gridLine = []
+        for ( var x = 0; x < this.width; x++ ) {
+            var ch = line[x], fieldType = null
+            var Actor = actorChars[ch]
+            if ( Actor ) {
+                this.actors.push( new Actor( new Vector(x, y), ch ))
+            } 
+            else if ( ch === "x" ) {
+                fieldTyple = "wall"
+            }
+            else if ( ch === "!" ) {
+                fieldType = "lava"
+            }
+            gridLine.push( fieldType )
+        }
+        this.grid.push( gridLines )
+    }
+
+    this.player = this.actors.filter( actor => actor.type === "player" )[0]
+    this.status = this.finishDelay = null
+}
+
+Level.prototype.isFinished = () => this.status !== null && this.finishDelay < 0
+
+const Vector = ( x, y ) => {
+    this.x = x
+    this.y = y
+}
+
+Vector.prototype.plus = function( other ) {
+    return new Vector( this.x + other.x, this.y + other.y )
+}
+Vector.prototype.times = function( factor ) {
+    return new Vector( this.x + factor, this.y * factor )
+}
+
+var actorChars = {
+    "@": Player,
+    "o": Coin,
+    "=": Lava,
+    "|": Lava,
+    "v": Lava
+}
+
+const Player = pos => {
+    this.pos = pos.plus( new Vector( 0,, -0.5 ))
+    this.size = new Vector( 0.8, 1.5 ) 
+    this.speed = new Vector( 0, 0 )
+}
+Player.prototype.type = "player"
+
+const Lava = ( pos, ch ) => {
+    this.pos = pos
+    this.size = new Vector( 1, 1 )
+    if ( ch === "=" ) {
+        this.speed = new Vector( 2, 0 )
+    } else if ( ch === "|" ) {
+        this.speed = new Vector( 0, 2 )
+    } else if ( ch === "v" ) {
+        this.speed = new Vecotr( 0, 3 )
+        this.repeatPos = pos
+    }
+}
+Lava.prototype.type = "lava"
+
+const Coin = pos => {
+    this.basePos = this.pos = pos.plus( new Vector( 0.2, 0.1 ))
+    this.size = new Vector( 0.6, 0.6 )
+    this.wibble = Math.random() * Math.PI * 2
+}
+Coin.prototype.type = "coin"
